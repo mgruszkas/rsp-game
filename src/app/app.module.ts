99ec01d04555089e58f2daba6c19e67f041e2f3f
michal.gruszka <michal.gruszka@accenture.com>
2017-05-04 09:15:48 +0200

    empty components - game, hand, shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HandComponent } from './game/hand/hand.component';
+import { ShapeComponent } from './game/shape/shape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HandComponent,
+    ShapeComponent
   ],
   imports: [
     BrowserModule,
