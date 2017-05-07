c04d7d265bc027af01042b0eea93e03431d70886
michal <m9ruszka@gmail.com>
2017-05-07 09:31:21 +0200

    unit tests for app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,64 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { ShapeComponent, HandComponent } from './index';
+import { AIPlayerService } from './services/aiplayer.service';
+
+@NgModule({
+  declarations: [HandComponent],
+  entryComponents: [
+    HandComponent
+  ],
+  providers: [],
+  exports: [HandComponent]
+})
+class TestModule {}
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        GameComponent,
+        ShapeComponent
       ],
+      providers: [
+        AIPlayerService
+      ],
+      imports: [TestModule],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+  it('should render "rsp-game" component', async(() => {
+    expect(compiled.querySelector('rsp-game')).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  it('should have Start button', async (() => {
+    expect(compiled.querySelector('.rsp-game-controls button.control-button')).toBeTruthy();
   }));
+
+  it('should have three shapes to choose from', async (() => {
+    expect(compiled.querySelectorAll('rsp-shape').length).toEqual(3);
+  }));
+  
 });
